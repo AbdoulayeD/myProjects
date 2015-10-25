e6f0239b4bd79d38ba6e43482610b552700a93d5
Abdoulaye Diop <2491raf@gmail.com>
2015-10-25 19:41:28 +0100

    kernel variable

--- Convolution/convolution.h
+++ Convolution/convolution.h
@@ -19,8 +19,8 @@
 template<typename T>
 class Convolution
 {
-    
-    
+
+
 protected:
     Image<T> cible;
     int     nkernel;
@@ -39,16 +39,16 @@ std::vector<T> decalage()
     kernel[index(0,0,3)]=0;
     kernel[index(0,1,3)]=1;
     kernel[index(0,2,3)]=0;
-    
+
     kernel[index(1,0,3)]=0;
     kernel[index(1,1,3)]=0;
     kernel[index(1,2,3)]=0;
-    
+
     kernel[index(2,0,3)]=0;
     kernel[index(2,1,3)]=0;
     kernel[index(2,2,3)]=0;
-    
-    
+
+
     return kernel;
 }
 
@@ -56,20 +56,20 @@ std::vector<T> decalage()
 template<typename T>
 std::vector<T> sharpen()
 {
-            std::vector<T> kernel(9);
-            kernel[index(0,0,3)]=-1;
-            kernel[index(0,1,3)]=-1;
-            kernel[index(0,2,3)]=-1;
-    
-            kernel[index(1,0,3)]=-1;
-            kernel[index(1,1,3)]=9;
-            kernel[index(1,2,3)]=-1;
-    
-            kernel[index(2,0,3)]=-1;
-            kernel[index(2,1,3)]=-1;
-            kernel[index(2,2,3)]=-1;
-    
-    
+    std::vector<T> kernel(9);
+    kernel[index(0,0,3)]=-1;
+    kernel[index(0,1,3)]=-1;
+    kernel[index(0,2,3)]=-1;
+
+    kernel[index(1,0,3)]=-1;
+    kernel[index(1,1,3)]=9;
+    kernel[index(1,2,3)]=-1;
+
+    kernel[index(2,0,3)]=-1;
+    kernel[index(2,1,3)]=-1;
+    kernel[index(2,2,3)]=-1;
+
+
     return kernel;
 }
 template<typename T>
@@ -79,23 +79,23 @@ std::vector<T> motionblur()
     kernel[index(0,0,3)]=1;
     kernel[index(0,1,3)]=0;
     kernel[index(0,2,3)]=0;
-    
+
     kernel[index(1,0,3)]=0;
     kernel[index(1,1,3)]=1;
     kernel[index(1,2,3)]=0;
-    
+
     kernel[index(2,0,3)]=0;
     kernel[index(2,1,3)]=0;
     kernel[index(2,2,3)]=1;
-    
-    
+
+
     return kernel;
 }
 template<typename T>
 std::vector<T> blur(int a)
 {
     std::vector<T> kernel(a);
-    
+
     return kernel;
 }
 
@@ -104,20 +104,22 @@ std::vector<T> blur(int a)
 template<typename T>
 Convolution <T>::Convolution(Image<T> a): cible(a)
 {
+    int n;
     int h=cible.geth();
     int w=cible.getw();
     int mv=cible.getmv();
     nkernel=3;
-    std::vector<T> kernel(nkernel*nkernel);
+    n=nkernel*nkernel;
+    std::vector<T> kernel(n);
     kernel=motionblur<T>();
-    
+
     std::cout<<std::endl<<"---->Try test:"<<std::endl;
     std::cout<<"Kernel:"<<nkernel<<std::endl;
     for(int i=0;i<nkernel;i++){
         for(int j=0;j<nkernel;j++)
             std::cout<<kernel[index(i,j,nkernel)]<<" ";
         std::cout<<std::endl;
-        
+
     }
     std::cout<<"Image:"<<std::endl;
     std::cout<<"w:"<<w<<std::endl;
@@ -127,44 +129,52 @@ Convolution <T>::Convolution(Image<T> a): cible(a)
         for(int j=0;j<w;j++)
             std::cout<<a(i,j)<<" ";
         std::cout<<std::endl;
-        
+
     }
-    
     /*for(int x = 0; x < w; x++)
         for(int y = 0; y < h; y++)
         {
             T newnc;
-            
             for(int filterX = 0; filterX <nkernel; filterX++)
                 for(int filterY = 0;filterY<nkernel; filterY++)
                 {
                     int imageX = (x-nkernel /2 + filterX + w) % w;
                     int imageY = (y-nkernel /2 + filterY + h) % h;
                     newnc += a(imageX,imageY)* kernel[index(filterX,filterY,nkernel)];
-
                 }
-            
             cible(x,y) = (newnc > mv) ?  mv : newnc ;
-            
-          
         }*/
-    //int x=0;
-    //int y=0;
-            for(int x = 0; x < h; x++)
-                for(int y = 0; y < w; y++)
+    /*for(int x = 0; x < h; x++)
+        for(int y = 0; y < w; y++)
+        {
+            if(x==0 || (x == h-1) || y==0 || (y == w-1))
+                {   }
+            else
+            {
+                cible(x,y)=a(x,y)*kernel[4] + a(x-1,y)*kernel[3] + a(x+1,y)*kernel[5] + a(x,y+1)*kernel[7] + a(x,y-1)*kernel[1]+
+                        a(x-1,y-1)*kernel[0] + a(x-1,y+1)*kernel[6] + a(x+1,y-1)*kernel[2] + a(x+1,y+1)*kernel[8];
+
+                cible(x,y) = (cible(x,y) > mv) ?  mv : cible(x,y);
+                cible(x,y) = (cible(x,y) > 0) ? cible(x,y): cible(x,y)*-1;
+            }
+        }
+        */
+        for(int x = 0; x < h ; x++)
+            for(int y = 0; y < w; y++)
+            {
+                T newval=0;
+                for(int kx=-1;kx<2;kx++)
+                for(int ky=-1;ky<2;ky++)
                 {
-                	if(x==0 || (x == h-1) || y==0 || (y == w-1))
-                    {   }
-                    else
-                    {
-                        cible(x,y)=a(x,y)*kernel[4] + a(x-1,y)*kernel[3] + a(x+1,y)*kernel[5] + a(x,y+1)*kernel[7] + a(x,y-1)*kernel[1]+
-                                        a(x-1,y-1)*kernel[0] + a(x-1,y+1)*kernel[6] + a(x+1,y-1)*kernel[2] + a(x+1,y+1)*kernel[8];
-                    
-                        cible(x,y) = (cible(x,y) > mv) ?  mv : cible(x,y);
-                    }
+                    int imagex=(x+kx)%h;
+                    int imagey=(y+ky)%w;
+
+                    newval+=a(imagex,imagey)*kernel[n/2 + kx + nkernel*ky];
                 }
-    
-    
+                cible(x,y) = (newval > mv) ?  mv : newval ;
+                cible(x,y) = (cible(x,y) > 0) ? cible(x,y): cible(x,y)*-1;
+            }
+
     std::cout<<std::endl<<"---->Try test 2:"<<std::endl;
     std::cout<<"Image:"<<std::endl;
     std::cout<<"w:"<<w<<std::endl;
@@ -175,7 +185,7 @@ Convolution <T>::Convolution(Image<T> a): cible(a)
         std::cout<<std::endl;
     }
     std::string output="output.pgm";
-    
+
     std::ofstream file(output,std::ios::out);
     if(file)
     {
@@ -186,15 +196,12 @@ Convolution <T>::Convolution(Image<T> a): cible(a)
             for(int j=0;j<w;j++)
                 file <<cible(i,j)<<" ";
             file<<std::endl;
-        
+
         }
-        
+
         file.close();
     }
     //std::ifstream file(imgname, std::ios::in);
-        
-
-    
 }
 
 
@@ -202,10 +209,6 @@ Convolution <T>::Convolution(Image<T> a): cible(a)
 template<typename T>
 void Convolution<T>::save(std::string a)
 {
-    
+
 }
-
-
-
-
 #endif /* Convolution_h */
