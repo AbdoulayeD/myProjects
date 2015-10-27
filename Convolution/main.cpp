cf349d2fd974addf79decedeeee60385e98bd228
Abdoulaye Diop <2491raf@gmail.com>
2015-10-27 23:35:52 +0100

    Mixte OpenMp and Mpi Implementation

--- Convolution/main.cpp
+++ Convolution/main.cpp
@@ -22,49 +22,50 @@ int main(int argc, char ** argv) {
     std::string outputFilename="output";
     std::string extention=".pgm";
     std::string outputfile =outputFilename+extention;
-    //std::cout<<std::endl<<"I am process number:"<<nrank<<std::endl;
     //Chargement Image
     Image<int> img("lena.ascii.pgm");
        if(nrank==0){
         std::cout<<"Image width : "<<img.getw()<<std::endl;
         std::cout<<"Image height : "<<img.geth()<<std::endl;
     }
-    
-    Q = img.geth()/nproc;
-    R = img.geth()%nproc;
-    struct infop infopip;
-    infopip.rank  = nrank;
-    infopip.nproc = nproc;
-    
-    /*infopip.nloc  = img.getw()/nproc;
-    infopip.ideb  = nrank*infopip.nloc;
-    infopip.ifin  = infopip.ideb+infopip.nloc;
-    */
-    if (infopip.rank < R) {
-        
-        infopip.nloc = Q+1;
-        infopip.ideb =infopip.rank * (Q+1);
-        infopip.ifin =infopip.ideb + infopip.nloc;
-        
-    } else {
-        
-        infopip.nloc = Q;
-        infopip.ideb =R * (Q+1) + (infopip.rank - R) * Q;
-        infopip.ifin =infopip.ideb + infopip.nloc;
-    }
-
-
     //Convolution
     double fact=1.0;
     double bia=0.0;
+    if(nproc>1)
+    {
+        Q = img.geth()/nproc;
+        R = img.geth()%nproc;
+        struct infop infopip;
+        infopip.rank  = nrank;
+        infopip.nproc = nproc;
+
+        /*infopip.nloc  = img.getw()/nproc;
+        infopip.ideb  = nrank*infopip.nloc;
+        infopip.ifin  = infopip.ideb+infopip.nloc;
+        */
+        if (infopip.rank < R) {
+
+            infopip.nloc = Q+1;
+            infopip.ideb =infopip.rank * (Q+1);
+            infopip.ifin =infopip.ideb + infopip.nloc;
+
+        } else {
+
+            infopip.nloc = Q;
+            infopip.ideb =R * (Q+1) + (infopip.rank - R) * Q;
+            infopip.ifin =infopip.ideb + infopip.nloc;
+        }
+        Convolution<float> convol(img,edges<float>(3),fact,bia,infopip);
+        if(nrank == 0)
+            convol.save(outputfile);
+    }else{
     //Convolution<int> convol(img);
     //Convolution<float> convol(img,fact, bia);
     //Convolution<int> convol(img,edges<int>(9));
-    //Convolution<float> convol(img,sharpen<float>(3),fact,bia);
-    Convolution<float> convol(img,edges<float>(3),fact,bia,infopip);
-    if(nrank == 0)
+        Convolution<float> convol(img,sharpen<float>(3),fact,bia);
         convol.save(outputfile);
-    
+    }
+
     MPI_Finalize();
     return 0;
 }
